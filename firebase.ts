b85213665aed8d0f82e7f813fe1ef9d32e317073
Zaid Siddique <zaidyousuf.siddique3@gmail.com>
2025-12-09 09:44:32 +0300

    Update firebase.ts to use env vars

--- firebase.ts
+++ firebase.ts
@@ -5,13 +5,13 @@ import { getStorage } from "firebase/storage";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCR1_K_R5FcPZsm0EN3Z5o9RWtS9Ro8fvo",
-  authDomain: "statement-of-account-dd2fc.firebaseapp.com",
-  projectId: "statement-of-account-dd2fc",
-  storageBucket: "statement-of-account-dd2fc.firebasestorage.app",
-  messagingSenderId: "513518376156",
-  appId: "1:513518376156:web:7660c73e76b097c2fa9019",
-  measurementId: "G-PJ16WY72VM"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
